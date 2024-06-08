5226141c42c85ea458f1e36d054a8d61830c2ef3
rraj786 <70361224+rraj786@users.noreply.github.com>
2024-06-08 22:57:33 +1200

    Training accuracy and loss stats fixed

--- tflow_model.py
+++ tflow_model.py
@@ -5,7 +5,9 @@
 '''
 
 
+import numpy as np
 import tensorflow as tf
+from tensorflow.keras.callbacks import History
 
 
 class TflowModel:
@@ -30,22 +32,18 @@ class TflowModel:
 
         # For first hidden layer, consider weights and biases between input and itself
         if len(self.hidden_layer_sizes) == 1:
-            self.model.add(tf.keras.layers.Dense(hidden_size, activation = 'relu', 
-                                                 kernel_initializer = self.weights_initialiser, 
-                                                 bias_initializer = self.biases_initialiser, 
-                                                 input_shape = (self.input_size,)))
+            self.model.add(tf.keras.layers.Dense(hidden_size, activation = 'relu', kernel_initializer = self.weights_initialiser, 
+                                                 bias_initializer = self.biases_initialiser, input_shape = (self.input_size,)))
         
         # For other hidden layers
         else:
-            self.model.add(tf.keras.layers.Dense(hidden_size, activation = 'relu', 
-                                                 kernel_initializer = self.weights_initialiser, 
+            self.model.add(tf.keras.layers.Dense(hidden_size, activation = 'relu', kernel_initializer = self.weights_initialiser, 
                                                  bias_initializer = self.biases_initialiser))
     
     def add_output_layer(self, output_size):
 
         # Add weights and biases for output layer
-        self.model.add(tf.keras.layers.Dense(output_size, activation = 'softmax', 
-                                             kernel_initializer = self.weights_initialiser,
+        self.model.add(tf.keras.layers.Dense(output_size, activation = 'softmax', kernel_initializer = self.weights_initialiser,
                                              bias_initializer = self.biases_initialiser))
 
     def train_network(self, x_train, y_train, batch_size, learning_rate, decay_rate, epochs):
@@ -53,18 +51,32 @@ class TflowModel:
         # Compile the model
         optimizer = tf.keras.optimizers.RMSprop(learning_rate = learning_rate, rho = decay_rate)
         self.model.compile(optimizer = optimizer,
-                    loss='categorical_crossentropy',
-                    metrics=['accuracy'])
+                    loss = 'categorical_crossentropy',
+                    metrics = ['accuracy'])
+
+        # Define a History callback to record training metrics
+        history = History()
 
         # Train the model
-        self.model.fit(x_train, y_train, batch_size = batch_size, epochs = epochs)
+        self.model.fit(x_train, y_train, batch_size = batch_size, epochs = epochs, callbacks = [history])
 
         # Print model summary
         self.model.summary()
 
-    def evaluate_model(self, x_test, y_test):
+        # Extract training accuracies and losses for each epoch
+        self.training_accuracies = history.history['accuracy']
+        self.training_losses = history.history['loss']
 
-        # Evaluate the model
-        loss, accuracy = self.model.evaluate(x_test, y_test)
-        print(f'Test Accuracy: {accuracy}')
-        print(f'Test Lccuracy: {loss}')
+    def predict(self, x_test):
+
+        # Predict on specified dataset
+        probabilities = self.model.predict(x_test)
+
+        # Extract confidence scores for predicted labels
+        confidence_scores = np.max(probabilities, axis = 1)
+
+        # Extract predicted labels
+        y_pred = np.argmax(probabilities, axis = 1)
+
+        return confidence_scores, y_pred
+    
